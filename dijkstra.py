3f7b4fe1b0ab7fd7a871f56bbb6d01943714c900
Stephen Rhoades <serhoades1@crimson.ua.edu>
2026-03-05 13:28:03 -0600

    Started dijkstra implementation

---
+++ dijkstra.py
@@ -0,0 +1,11 @@
+Visited = []
+Distance = []
+
+def dijkstraAlgo():
+    Distance[1] = 0
+    for i in range : # FIXME: Add range for all edges
+        Distance[i] = 0 # Sum all vertices + 1
+
+    # Check all nodes immediately next to current node
+    # Change total expected burn time to current computed burn time
+    # Only do so if burn time of current node plus weight of the edge from current node to next node is less that current burn time of next node
